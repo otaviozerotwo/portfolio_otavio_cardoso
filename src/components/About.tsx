8cac199a7ddd3b82d49af583ed94d49c7f081aea
Otavio Cardoso <otavio.cardoso@rfn.ptc>
2025-08-26 08:28:28 -0300

    feat: add texto About component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,7 +4,17 @@ export function About() {
   return (
     <section>
       <Container>
-        <p>About Section</p>
+        <h2>Sobre Mim</h2>
+
+        <p>
+          Sou graduando em Análise e Desenvolvimento de Sistemas, com 31 anos e uma paixão crescente pelo desenvolvimento web. Desde o início da minha jornada, tenho me especializado na stack JavaScript, explorando tanto o front-end quanto o back-end para criar soluções eficientes e modernas.
+        </p>
+        <p>
+          Com um olhar atento aos detalhes e uma dedicação contínua ao aprimoramento técnico, estou sempre em busca de novas oportunidades para expandir meus conhecimentos e entregar projetos que superem as expectativas.
+        </p>
+        <p>
+          Se você procura alguém comprometido, responsável e com grande entusiasmo pela tecnologia, posso ser a escolha certa para o seu projeto.
+        </p>
       </Container>
     </section>
   );  
